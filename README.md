8b42078e80bce8b010681b331af016857251b459
Paul Sauve <paul@burngames.net>
2015-07-11 23:10:46 -0500

    License and a few fixes

---
+++ README.md
@@ -0,0 +1,6 @@
+Lambda Promises
+===
+
+This is the start to a fast Java implementation of Promises.
+
+No warranty includes, licensed under MIT.
--- src/main/java/net/burngames/lp/types/tasks/TripleTask.java
+++ src/main/java/net/burngames/lp/types/tasks/TripleTask.java
@@ -6,7 +6,5 @@ package net.burngames.lp.types.tasks;
  * @author PaulBGD
  */
 public abstract class TripleTask<T, U, V> {
-
     public abstract Object task(T object, U object2, V object3);
-
 }
